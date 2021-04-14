35ff9153118070aefecd0f9c96a02b18d9626414
DBKynd <db@dbkynd.com>
2021-04-14 16:31:50 -0600

    job data table

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,64 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
-  </div>
+  <v-row>
+    <v-data-table :items="jobs" :headers="headers"> </v-data-table>
+  </v-row>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Job #',
+          align: 'start',
+          sortable: true,
+          value: 'id',
+        },
+        {
+          text: 'Part #',
+          sortable: true,
+          value: 'partName',
+        },
+        {
+          text: 'Customer',
+          sortable: true,
+          value: 'customer',
+        },
+        {
+          text: 'Qty',
+          value: 'quantity',
+        },
+        {
+          text: 'Due Date',
+          value: 'dueDate',
+        },
+      ],
+      jobs: [
+        {
+          id: '1000',
+          partName: 'Part X',
+          customer: 'Some Customer',
+          quantity: 1000,
+          dueDate: '5/15/2002',
+        },
+        {
+          id: '1001',
+          partName: 'Part Y',
+          customer: 'Some Customer',
+          quantity: 100,
+          dueDate: '5/15/2002',
+        },
+        {
+          id: '1005',
+          partName: 'Part Z',
+          customer: 'Some Customer',
+          quantity: 300,
+          dueDate: '5/15/2002',
+        },
+      ],
+    }
+  },
 }
 </script>
