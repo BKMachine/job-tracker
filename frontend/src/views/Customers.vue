9e7722e72f93b67302bd4c1395b7729c00a4a2e6
DBKynd <db@dbkynd.com>
2021-04-16 15:10:33 -0600

    remove div container

--- frontend/src/views/Customers.vue
+++ frontend/src/views/Customers.vue
@@ -1,136 +1,134 @@
 <template>
-  <div>
-    <v-container>
-      <v-row justify="center" class="mt-10">
-        <v-col cols="10">
-          <v-data-table
-            :headers="headers"
-            :items="customers"
-            sort-by="name"
-            class="elevation-2"
-            :search="search"
-            hide-default-footer
-            disable-pagination
-            :loading="loading"
-          >
-            <template v-slot:top>
-              <v-toolbar flat>
-                <v-toolbar-title>Customers</v-toolbar-title>
-                <v-spacer />
-                <v-text-field
-                  v-model="search"
-                  append-icon="mdi-magnify"
-                  label="Search"
-                  single-line
-                  hide-details
-                  class="mr-3"
-                  :clearable="true"
-                />
-                <v-dialog v-model="editDialog" persistent max-width="600px">
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-btn color="primary" dark v-bind="attrs" v-on="on">
-                      New Customer
+  <v-container>
+    <v-row justify="center" class="mt-10">
+      <v-col cols="10">
+        <v-data-table
+          :headers="headers"
+          :items="customers"
+          sort-by="name"
+          class="elevation-2"
+          :search="search"
+          hide-default-footer
+          disable-pagination
+          :loading="loading"
+        >
+          <template v-slot:top>
+            <v-toolbar flat>
+              <v-toolbar-title>Customers</v-toolbar-title>
+              <v-spacer />
+              <v-text-field
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="Search"
+                single-line
+                hide-details
+                class="mr-3"
+                :clearable="true"
+              />
+              <v-dialog v-model="editDialog" persistent max-width="600px">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                    New Customer
+                  </v-btn>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="headline">{{ formTitle }}</span>
+                  </v-card-title>
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col>
+                          <v-text-field
+                            v-model="editedItem.name"
+                            label="Name"
+                            required
+                            :autofocus="editedIndex === -1"
+                          />
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col>
+                          <v-text-field
+                            v-model="editedItem.address"
+                            label="Address"
+                          />
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col cols="6">
+                          <v-text-field
+                            v-model="editedItem.contact"
+                            label="Contact"
+                          />
+                        </v-col>
+                        <v-col cols="6">
+                          <VuePhoneNumberInput
+                            v-model="editedItem.phone"
+                            :no-country-selector="true"
+                          />
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-btn color="gray darken-1" text @click="close">
+                      Cancel
                     </v-btn>
-                  </template>
-                  <v-card>
-                    <v-card-title>
-                      <span class="headline">{{ formTitle }}</span>
-                    </v-card-title>
-                    <v-card-text>
-                      <v-container>
-                        <v-row>
-                          <v-col>
-                            <v-text-field
-                              v-model="editedItem.name"
-                              label="Name"
-                              required
-                              :autofocus="editedIndex === -1"
-                            />
-                          </v-col>
-                        </v-row>
-                        <v-row>
-                          <v-col>
-                            <v-text-field
-                              v-model="editedItem.address"
-                              label="Address"
-                            />
-                          </v-col>
-                        </v-row>
-                        <v-row>
-                          <v-col cols="6">
-                            <v-text-field
-                              v-model="editedItem.contact"
-                              label="Contact"
-                            />
-                          </v-col>
-                          <v-col cols="6">
-                            <VuePhoneNumberInput
-                              v-model="editedItem.phone"
-                              :no-country-selector="true"
-                            />
-                          </v-col>
-                        </v-row>
-                      </v-container>
-                    </v-card-text>
-                    <v-card-actions>
-                      <v-btn color="gray darken-1" text @click="close">
-                        Cancel
-                      </v-btn>
-                      <v-spacer />
-                      <v-btn color="blue darken-1" text @click="saveItem">
-                        {{ actionTitle }}
-                      </v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog>
-                <v-dialog v-model="dialogDelete" max-width="500px">
-                  <v-card>
-                    <v-card-title class="headline">
-                      {{ editedItem.name }}
-                    </v-card-title>
-                    <v-card-text>
-                      Are you sure you want to delete this customer?
-                    </v-card-text>
-                    <v-card-actions>
-                      <v-spacer />
-                      <v-btn color="grey darken-1" text @click="closeDelete">
-                        Cancel
-                      </v-btn>
-                      <v-btn
-                        color="blue darken-1"
-                        text
-                        @click="deleteItemConfirm"
-                      >
-                        OK
-                      </v-btn>
-                      <v-spacer />
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog>
-              </v-toolbar>
-            </template>
-            <template v-slot:item.actions="{ item }">
-              <v-row class="mr-0">
-                <v-spacer></v-spacer>
-                <v-icon
-                  v-if="item.address"
-                  small
-                  class="mr-2"
-                  @click="openMap(item)"
-                >
-                  mdi-map-marker
-                </v-icon>
-                <v-icon small class="mr-2" @click="editItem(item)">
-                  mdi-pencil
-                </v-icon>
-                <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
-              </v-row>
-            </template>
-          </v-data-table>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+                    <v-spacer />
+                    <v-btn color="blue darken-1" text @click="saveItem">
+                      {{ actionTitle }}
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+              <v-dialog v-model="dialogDelete" max-width="500px">
+                <v-card>
+                  <v-card-title class="headline">
+                    {{ editedItem.name }}
+                  </v-card-title>
+                  <v-card-text>
+                    Are you sure you want to delete this customer?
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer />
+                    <v-btn color="grey darken-1" text @click="closeDelete">
+                      Cancel
+                    </v-btn>
+                    <v-btn
+                      color="blue darken-1"
+                      text
+                      @click="deleteItemConfirm"
+                    >
+                      OK
+                    </v-btn>
+                    <v-spacer />
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-toolbar>
+          </template>
+          <template v-slot:item.actions="{ item }">
+            <v-row class="mr-0">
+              <v-spacer></v-spacer>
+              <v-icon
+                v-if="item.address"
+                small
+                class="mr-2"
+                @click="openMap(item)"
+              >
+                mdi-map-marker
+              </v-icon>
+              <v-icon small class="mr-2" @click="editItem(item)">
+                mdi-pencil
+              </v-icon>
+              <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+            </v-row>
+          </template>
+        </v-data-table>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
