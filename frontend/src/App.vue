b4ffedda5c25570728d28bc5a3dd5bfab0aa0505
DBKynd <db@dbkynd.com>
2021-05-27 09:15:27 -0600

    get usb devices

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,6 +13,19 @@ import NavDrawer from '@/components/NavDrawer'
 export default {
   name: 'App',
   components: { NavDrawer },
+  mounted() {
+    navigator.usb.getDevices().then((devices) => {
+      console.log('Total devices: ' + devices.length)
+      devices.forEach((device) => {
+        console.log(
+          'Product name: ' +
+            device.productName +
+            ', serial number ' +
+            device.serialNumber,
+        )
+      })
+    })
+  },
 }
 </script>
 
